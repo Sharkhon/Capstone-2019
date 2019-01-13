a27b00e52d9a48e84ea4c51d4763c9d612b569b5
Daniel Cater <dcater1@my.westga.edu>
2019-01-13 18:57:11 -0500

    Updated all of the nuget packages and added "login" functionality.

--- CaterCroweCapstone2019/Controllers/HomeController.cs
+++ CaterCroweCapstone2019/Controllers/HomeController.cs
@@ -8,9 +8,36 @@ namespace CaterCroweCapstone2019.Controllers
 {
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// Returns the Index for the home
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
+            if (HttpContext.Session["loginStatus"] == null)
+            {
+                HttpContext.Session.Add("loginStatus", false);
+            }
+
+            ViewBag.loggedIn = (bool)HttpContext.Session["loginStatus"];
+
             return View();
+        }
+
+        /// <summary>
+        /// Logs the user in
+        /// </summary>
+        /// <param name="username">Username to authenticate to</param>
+        /// <param name="password">Password to autheticate by</param>
+        /// <returns>The index with the loggedin</returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogIn(string username, string password)
+        {
+            HttpContext.Session.Add("loginStatus", true);
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
---
+++ CaterCroweCapstone2019/Scripts/scripts/login.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $('#loginButton').click(loginUser);
+
+    function loginUser() {
+        if (!validateLogin()) {
+            return;
+        }
+
+        var username = $('#username').text();
+        var password = $('#password').text();
+
+        $.post('/Home/Login',
+            {
+                username: username,
+                password: password
+            },
+            function () {
+                //TODO Actually check login 
+
+                location.reload();
+            });
+    }
+
+    function validateLogin() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        if (username.length === 0 || password.length === 0) {
+            $('#loginError').text('Fill out username and password.');
+            $('#loginError').css('display', 'block');
+            return false;
+        }
+
+        return true;
+    }
+});

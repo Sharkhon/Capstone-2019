88357bb7434715b0abc9fc7550fd49972ced608f
DanielJesseCater <DanielJesseCater@DESKTOP-2SNQ0A2>
2019-01-12 23:18:35 -0500

    Add project files.

---
+++ CaterCroweCapstone2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CaterCroweCapstone2019.Startup))]
+namespace CaterCroweCapstone2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
